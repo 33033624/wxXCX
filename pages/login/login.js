7ef5309aea61cfc242e26929b374ecdbbcdc3091
Macx <Macx@MacxdeMacBook-Pro-751.local>
2017-01-17 13:42:26 +0800

    修改微信反馈的bug

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,33 +1,29 @@
 Page({
-    data: {
-    remind: '加载中',
-    help_status: false,
-    userid_focus: false,
-    passwd_focus: false,
-    userid: '',
-    passwd: '',
-    angle: 0
-  },
-  onReady: function(){
-    var _this = this;
-    setTimeout(function(){
-      _this.setData({
-        remind: ''
-      });
-    }, 1000);
-    wx.onAccelerometerChange(function(res) {
-      var angle = -(res.x*30).toFixed(1);
-      if(angle>14){ angle=14; }
-      else if(angle<-14){ angle=-14; }
-      if(_this.data.angle !== angle){
+      data: {
+      remind: '加载中',
+      angle: 0
+    },
+    onReady: function(){
+      var _this = this;
+      setTimeout(function(){
         _this.setData({
-          angle: angle
+          remind: ''
         });
-      }
-    });
-  },
+      }, 1000);
+      //重力感应
+      wx.onAccelerometerChange(function(res) {
+        var angle = -(res.x*30).toFixed(1);
+        if(angle>14){ angle=14; }
+        else if(angle<-14){ angle=-14; }
+        if(_this.data.angle !== angle){
+          _this.setData({
+            angle: angle
+          });
+        }
+      });
+    },
     onTap: function (event) {
-        wx.redirectTo({
+        wx.navigateTo({
             url: "../movies/movies"
         });
         
@@ -36,7 +32,7 @@ Page({
         return {
         title: '找好电影推荐',
         desc: '进入搜索电影吧',
-        path: '/pages/welcome/welcome'
+        path: '/pages/login/login'
         }
   }
 })
